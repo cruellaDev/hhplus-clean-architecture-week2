65209d7c99744400b2e341ffc7fc3a160e3d7c5c
cruellaDev <cruellaDevLee@gmail.com>
2024-06-22 20:14:26 +0900

    init

---
+++ register-lecture/src/test/java/com/io/hhplus/tdd/registerlecture/RegisterLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.hhplus.tdd.registerlecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterLectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
